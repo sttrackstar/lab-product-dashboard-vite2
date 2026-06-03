876fb24c13b5529c7a0de654ecd52760523ed277
sttrackstar <sonalt321@gmail.com>
2026-06-03 15:54:15 -0400

    Assignment completed

--- jest.config.js
+++ jest.config.js
@@ -4,3 +4,10 @@ module.exports = {
   },
   testEnvironment: 'jsdom',
 }
+const { JSDOM } = require('jsdom')
+const dom = new JSDOM()
+global.window = dom.window
+global.document = dom.window.document
+global.navigator = {
+  userAgent: 'node.js',
+}
